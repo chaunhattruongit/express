e675823f73ac8b283ccb6f0f38b83d9b5206bf71
chaunhattruongit <chaunhattruongit@gmail.com>
2018-08-30 20:34:39 +0700

    done 2

--- routes/truong.js
+++ routes/truong.js
@@ -4,12 +4,11 @@ var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'FullStack Vietnam' });
 	var obj;
-	fs.readFile('./db/home.json', 'utf8', function (err, data) {
+	fs.readFile('./db/truong.json', 'utf8', function (err, data) {
 		if (err) throw err;
 		obj = JSON.parse(data);
-		res.render('index', { dataTruong:obj });
+		res.render('truong', { truong: obj });
 	});
 });
 
